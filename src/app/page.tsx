ed035a73e1c745eba9b64e4b4e086dc705c861f4
Sungjun Lee <253eosam@gmail.com>
2025-05-19 22:37:31 +0900

    배포 환경 설정 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client'
+
+import { useState } from 'react'
 
 export default function Home() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      Hello 
+    <div className="h-dvh flex flex-col gap-y-4 justify-center items-center">
+      <p> {count} </p>
+      <button className="border p-4 rounded-md hover:bg-gray-600 hover:text-white" onClick={() => setCount((pre) => pre + 1)}>
+        add button
+      </button>
     </div>
-  );
+  )
 }
