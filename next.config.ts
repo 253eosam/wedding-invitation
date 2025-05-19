739fc63d25bf36ba8ac80aee517a2074dad15f85
Sungjun Lee <253eosam@gmail.com>
2025-05-19 22:58:09 +0900

    basePath 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/wedding-invitation',
+  assetPrefix: '/wedding-invitation/',
 }
 
 export default nextConfig
