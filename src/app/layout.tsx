6f8185e15b9fdeead733fc3f93fb144aa00d99c6
Sungjun Lee <253eosam@gmail.com>
2025-05-23 00:03:29 +0900

    section 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
-import { Crimson_Pro } from "next/font/google";
+import { Crimson_Pro, Gowun_Dodum } from "next/font/google";
 import "@/app/globals.css";
 import { title, description } from "@/assets/DB.json";
 
 const CrimsonPro = Crimson_Pro({
   variable: "--font-crimson-pro",
   subsets: ["latin"],
+});
+
+const GowunDodum = Gowun_Dodum({
+  variable: "--font-gowun-dodum",
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -21,7 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={` ${CrimsonPro.variable} antialiased bg-[#efefef]`}>
+      <body
+        className={`${CrimsonPro.variable} ${GowunDodum.variable} antialiased bg-[#efefef]`}
+      >
         <main className="w-[425px] bg-[#fafafa] mx-auto" role="main">
           {children}
         </main>
--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -3,19 +3,28 @@ import { weddingDate, description, names } from "@/assets/DB.json";
 export default function MainSection() {
   return (
     <section>
-      <div>
-        <h1>{`${weddingDate.year}/${weddingDate.month}/${weddingDate.day}`}</h1>
-        <p>{weddingDate.dayOfWeek}</p>
+      <div className="flex flex-col items-center font-crimson mt-10 gap-1 text-gray-600">
+        <h1 className="text-[30px]">{`${weddingDate.year} / ${String(weddingDate.month).padStart(2, "0")} / ${weddingDate.day}`}</h1>
+        <p className="text-base uppercase tracking-[2px]">
+          {weddingDate.dayOfWeek}
+        </p>
       </div>
-      <div>
-        <img src="https://placehold.co/425x585" alt="메인 이미지" />
+      <div className="my-15">
+        <img
+          src="https://placehold.co/425x585"
+          alt="메인 이미지"
+          className="rounded-2xl"
+        />
       </div>
-      <div>
-        <span>{names.groom.me}</span>
-        <span>{names.bride.me}</span>
-      </div>
-      <div>
-        <p>{description}</p>
+      <div className="flex flex-col items-center gap-y-5 font-gowun">
+        <div className="flex flex-row gap-2.5 text-lg">
+          <span>{names.groom.me}</span>
+          <span>·</span>
+          <span>{names.bride.me}</span>
+        </div>
+        <p className="whitespace-pre text-center text-base text-gray-600">
+          {description}
+        </p>
       </div>
     </section>
   );
