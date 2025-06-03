b70653046189cf3dd24bb445ccac359ceaddcf2e
Sungjun Lee <253eosam@gmail.com>
2025-06-03 20:53:02 +0900

    데이터 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,17 @@ import GallerySection from '@/components/GallerySection'
 import CalendarSection from '@/components/CalendarSection'
 import MapSection from '@/components/MapSection'
 import AccountSection from '@/components/AccountSection'
+import DB from '@/assets/DB.json'
 
 export default function Home() {
+  const { weddingDate } = DB
+
   return (
     <div className="flex flex-col gap-y-15 py-15">
       <MainSection />
       <IntroSection />
       <GallerySection />
-      <CalendarSection />
+      <CalendarSection weddingDate={weddingDate} />
       <MapSection />
       <AccountSection />
     </div>
