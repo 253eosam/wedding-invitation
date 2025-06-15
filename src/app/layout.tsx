d3f8c7b734d0c214b14dcc921657aa3ae93c348e
Sungjun Lee <253eosam@gmail.com>
2025-06-15 23:05:23 +0900

    무료 favicon 추가
    - ref : https://icons8.kr/icons/set/favicon-flower

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body
         className={`${CrimsonPro.variable} ${GowunDodum.variable} ${NotoSansKR.variable} antialiased bg-[#efefef]`}
       >
